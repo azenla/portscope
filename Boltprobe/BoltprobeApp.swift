2b5235c357fdf2408b638cfcd14c050e615c476d
Alex Zenla <alex@edera.dev>
2026-05-17 00:53:14 -0700

    Initial Commit

---
+++ Boltprobe/BoltprobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoltprobeApp.swift
+//  Boltprobe
+//
+//  Created by Alex Zenla on 5/17/26.
+//
+
+import SwiftUI
+
+@main
+struct BoltprobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boltprobe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boltprobe
+//
+//  Created by Alex Zenla on 5/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
